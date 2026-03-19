386587549fd2ccc37205e6098b260eb2a1eb7e08
Hello-Liu0618 <2463141541@qq.com>
2026-03-19 17:41:46 +0800

    用户登录拦截器优化

--- src/main/java/com/hmdp/interceptor/LoginInterceptor.java
+++ src/main/java/com/hmdp/interceptor/LoginInterceptor.java
@@ -14,22 +14,14 @@ public class LoginInterceptor implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        //从request中获取session
-        HttpSession session = request.getSession();
-        //获取user
-        Object user = session.getAttribute("user");
-
-        if ( user == null ) {
-            //设置状态为402
+        // 1.判断是否需要拦截（ThreadLocal中是否有用户）
+        if (UserHolder.getUser() == null) {
+            // 没有，需要拦截，设置状态码
             response.setStatus(401);
+            // 拦截
             return false;
         }
-        //创建UserDTO对象
-        UserDTO userDTO = new UserDTO();
-        BeanUtils.copyProperties(user,userDTO);
-
-        //将用户DTO信息保存到LocalThread中
-        UserHolder.saveUser(userDTO);
+        // 有用户，则放行
         return true;
     }
 }
